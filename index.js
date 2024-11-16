ca3fa7408a7ec9e49a4012ad1c4290082455ee86
Rebelgames992 <rebelgames992@gmail.com>
2024-11-16 18:27:28 +0000

    added config for custom token ring

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ Hooks.on("initializeDynamicTokenRingConfig", (a) => {
     },
     spritesheet: "modules/Custom-token-rings/rings/rings-blood.json"
   });
-  a.addConfig("bloodringstesting", s);
+  a.addConfig("bloodring", s);
 });
+
 Hooks.on("initializeDynamicTokenRingConfig", (a) => {
   const s = new foundry.canvas.tokens.DynamicRingData({
     label: "sky king's ring",
@@ -24,4 +25,19 @@ Hooks.on("initializeDynamicTokenRingConfig", (a) => {
   });
   a.addConfig("skykingsrings", s);
 });
+
+Hooks.on("initializeDynamicTokenRingConfig", (a) => {
+  const s = new foundry.canvas.tokens.DynamicRingData({
+    label: "custom ring",
+    effects: {
+      RING_PULSE: "TOKEN.RING.EFFECTS.RING_PULSE",
+      RING_GRADIENT: "TOKEN.RING.EFFECTS.RING_GRADIENT",
+      BKG_WAVE: "TOKEN.RING.EFFECTS.BKG_WAVE",
+      INVISIBILITY: "TOKEN.RING.EFFECTS.INVISIBILITY"
+    },
+    spritesheet: "modules/Custom-token-rings/rings/custom token ring.json"
+  });
+  a.addConfig("customtokenring", s);
+});
+
 console.log(`[${f}@${Z}...] successfully loaded!`);
